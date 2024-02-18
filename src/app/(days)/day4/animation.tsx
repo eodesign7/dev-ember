b8271f78139b080b7d4c4dcbaf430e7dd977820b
Erik Ondruš <ondrus.eodesign@gmail.com>
2024-02-18 07:33:46 +0100

    Day 4 ended at 12:12

--- src/app/(days)/day4/animation.tsx
+++ src/app/(days)/day4/animation.tsx
@@ -1,12 +1,23 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import LottieView from "lottie-react-native";
+import Netflix from "@/assets/lottie/netflix.json";
 
 export default function AnimationScreen() {
   return (
     <View>
-      <Text>
-         Animation Screen
-      </Text>
+      <LottieView
+        autoPlay
+        style={{
+          width: 200,
+          height: 200,
+          backgroundColor: "#eee",
+        }}
+        source={Netflix}
+      />
     </View>
-  )
+  );
 }
+
+
+// Day 4 Ended on 12:12 - 1:16:39
